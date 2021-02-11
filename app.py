0fb806988203177f405d7b0838abee83aaba0aad
MaityKrishanu <53084622+MaityKrishanu@users.noreply.github.com>
2021-02-12 00:54:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import model_from_json 
-import pickle5 as pickle
+import pickle
 app = Flask(__name__)
 #CORS(app)
 vocab_size = 12708
@@ -33,14 +33,20 @@ loaded_model.load_weights("model.h5")
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['GET','POST'])
+@app.route('/predict',methods=['GET','POST']) #'GET',
 def predict():
     '''
     For rendering results on HTML GUI
     '''
     temp=request.get_data(as_text=True)
     start = time.time()
-    new=[temp]
+    new_string = temp.replace("+", " ")
+    new_string = new_string.replace("%2C", " ")
+    new_string = new_string.replace("%3B", " ")
+    new_string = new_string.replace("text_input=", " ")
+    new_string= " ".join(new_string.split())
+    #print(temp)
+    new=[new_string]
     tok = tokenizer1.texts_to_sequences(new)
     pad = pad_sequences(tok, padding='post', maxlen=maxlen)
     y_pred1 = loaded_model.predict(pad)
@@ -49,13 +55,13 @@ def predict():
     if pred == 1:
         output = "Bully"
     else:
-        output = "Non_Bully"
+        output = "Non Bully"
 
     tt = end - start
-
-    return render_template('index.html', 
-                           prediction_text='Input sentence indicates : {}'.format(output)
-                           , Time_taken='Time Taken for prediction : {:.2f}'.format(tt),
+    cc = "all bal chal"
+    return render_template('index.html', Input_text1='Input Sentence : {}'.format(new_string),
+                           prediction_text='Input Sentence Indicates : {}'.format(output)
+                           , Time_taken='Time Taken for Prediction : {:.2f}'.format(tt),
                            nonbully_prob ='Non-Bully  Probability : {:.2f}'.format(y_pred1[0,0]),
                            bully_prob='Bully Probability : {:.2f}'.format(y_pred1[0,1]))
 
